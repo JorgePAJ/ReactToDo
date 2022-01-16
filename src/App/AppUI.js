6efb9477bb57907729cf9d9e8ab3dfa76e760137
Jorge Plasencia <66725751+WaberHoruhe@users.noreply.github.com>
2022-01-15 17:06:14 -0700

    decimo cuarta clase// applista

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from "react";
 import "./App.css";
-import { TodoContext } from '../TodoContext';
-import { TodoCounter } from '../TodoCounter';
-import { TodoSearch } from '../TodoSearch';
-import { TodoList } from '../TodoList';
-import { TodoItem } from '../TodoItem';
-import { CreateTodoButton } from '../CreateTodoButton';
-import { Modal } from '../Modal';
+import { TodoContext } from "../TodoContext";
+import { TodoCounter } from "../TodoCounter";
+import { TodoSearch } from "../TodoSearch";
+import { TodoList } from "../TodoList";
+import { TodoItem } from "../TodoItem";
+import { CreateTodoButton } from "../CreateTodoButton";
+import { Modal } from "../Modal";
+import { TodoForm } from "../TodoForm";
 
 function AppUI() {
   const {
@@ -22,29 +23,28 @@ function AppUI() {
     <React.Fragment>
       <TodoCounter />
       <TodoSearch />
-          <TodoList>
-            {error && <p>Desespérate, hubo un error...</p>}
-            {loading && <p>Estamos cargando, no desesperes...</p>}
-            {(!loading && !searchedTodos.length) && <p className='App-emptytodo'>¡Crea tu primer TODO!</p>}
-            {searchedTodos.map(todo => (
-              <TodoItem
-                key={todo.text}
-                text={todo.text}
-                completed={todo.completed}
-                onComplete={() => completeTodo(todo.text)}
-                onDelete={() => deleteTodo(todo.text)}
-              />
-            ))}
-          </TodoList>
-          {!!openModal && (
-            <Modal>
-            <p>{searchedTodos[0]?.text}</p>
-            </Modal>
-          )}
-      <CreateTodoButton 
-        setOpenModal = {setOpenModal}
-        openModal = {openModal}
-      />
+      <TodoList>
+        {error && <p>Desespérate, hubo un error...</p>}
+        {loading && <p>Estamos cargando, no desesperes...</p>}
+        {!loading && !searchedTodos.length && (
+          <p className="App-emptytodo">¡Crea tu primer TODO!</p>
+        )}
+        {searchedTodos.map((todo) => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
+        ))}
+      </TodoList>
+      {!!openModal && (
+        <Modal>
+          <TodoForm />
+        </Modal>
+      )}
+      <CreateTodoButton setOpenModal={setOpenModal} openModal={openModal} />
     </React.Fragment>
   );
 }
--- src/TodoItem/index.js
+++ src/TodoItem/index.js
@@ -3,6 +3,7 @@ import './TodoItem.css';
 
 function TodoItem(props) {
   return (
+
     <li className="TodoItem">
       <span
         className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
